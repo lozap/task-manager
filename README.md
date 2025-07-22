d79f94a9d74d8fc242be36e376510f8cf8f96e56
lozap <lozinskyy@gmail.com>
2025-07-22 15:55:46 +0300

    SO-127 Change comments in server.js, README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+<!--
+Опис команд роботи з платформою репозиторіїв github
+-->
+
+git config --global user.name "lozap"
+
+git config --global user.email "lozinskyy@gmail.com"
+
+git config --global init.defaultBranch main
+
+git init
+
+git add .
+
+git commit -m "Initial commit"
+
+touch README.md
+
+git commit -m "Add README"
+
+gh auth login
+
+gh repo create task-manager --public
+
+git branch -M main
+
+git remote add origin https://github.com/lozap/task-manager.git
+
+git remote set-url origin
+
+git push -u origin main
--- server.js
+++ server.js
@@ -1,18 +1,5 @@
+
 const sum = (a, b) => a + b;
 
 console.log(sum(5, 3)); // Outputs: 8
 
-// git config --global user.name "lozap"
-// git config --global user.email "lozinskyy@gmail.com"
-// git config --global init.defaultBranch main
-// git init
-// git add .
-// git commit -m "Initial commit"
-// touch README.md
-// git commit -m "Add README"
-// gh auth login
-// gh repo create task-manager --public
-// git branch -M main
-// git remote add origin https://github.com/lozap/task-manager.git
-// git remote set-url origin
-// git push -u origin main
