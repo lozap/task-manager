b7d42f243d3add8e7daf6a75b3579fea263482dc
lozap <lozinskyy@gmail.com>
2025-07-22 14:52:14 +0300

    SO-125 Chande server.js

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const sum = (a, b) => a + b;
 
 console.log(sum(5, 3)); // Outputs: 8
 
+// git config --global user.name "lozap"
+// git config --global user.email "lozinskyy@gmail.com"
+// git config --global init.defaultBranch main
+// git init
+// git add .
+// git commit -m "Initial commit"
+// git add README.md
+// git commit -m "Add README"
+// git branch -M main
+// gh repo create test --branch main --public
+// git remote add origin
+// git remote set-url origin
+// git push -u origin main
